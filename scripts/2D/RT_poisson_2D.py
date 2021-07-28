cf8ec906386f3d6a5322b273c53908613a8fe68e
Diego Tavares Volpatto <diego@esss.co>
2021-07-28 19:24:45 -0300

    Add 2D mixed Raviart-Thomas example

---
+++ scripts/2D/RT_poisson_2D.py
@@ -0,0 +1,111 @@
+from firedrake import *
+import matplotlib.pyplot as plt
+from firedrake.petsc import PETSc
+
+
+def print(content_to_print):
+    return PETSc.Sys.Print(content_to_print)
+
+
+PETSc.Log.begin()
+
+# Defining the mesh
+N = 10
+use_quads = False
+mesh = UnitSquareMesh(N, N, quadrilateral=use_quads)
+comm = mesh.comm
+
+# Function space declaration
+if use_quads:
+    hdiv_family = 'RTCF'
+    pressure_family = 'DQ'
+else:
+    hdiv_family = 'RT'
+    pressure_family = 'DG'
+
+degree = 1
+U = FunctionSpace(mesh, hdiv_family, degree + 1)
+V = FunctionSpace(mesh, pressure_family, degree)
+W = U * V
+
+# Trial and test functions
+u, p = TrialFunctions(W)
+v, q = TestFunctions(W)
+
+# Mesh entities
+n = FacetNormal(mesh)
+h = CellDiameter(mesh)
+x, y = SpatialCoordinate(mesh)
+
+# Exact solution
+p_exact = sin(2 * pi * x) * sin(2 * pi * y)
+exact_solution = Function(V).interpolate(p_exact)
+exact_solution.rename("Exact pressure", "label")
+sigma_e = Function(U, name='Exact velocity')
+sigma_e.project(-grad(p_exact))
+
+# Forcing function
+f_expression = div(-grad(p_exact))
+f = Function(V).interpolate(f_expression)
+
+# Dirichlet BCs (not necessary to impose in this case)
+# bcs = DirichletBC(W[0], sigma_e, "on_boundary")
+
+# Mixed classical terms
+a = (dot(u, v) - div(v) * p + q * div(u)) * dx
+L = f * q * dx - dot(v, n) * exact_solution * ds
+
+solver_parameters = {
+    "ksp_monitor": None,
+    "mat_type": "aij",
+    "ksp_type": "preonly",
+    "pc_type": "lu",
+    "pc_factor_mat_solver_type": "mumps",
+}
+solution = Function(W)
+problem = LinearVariationalProblem(a, L, solution, bcs=[])
+solver = LinearVariationalSolver(problem, solver_parameters=solver_parameters)
+solver.snes.ksp.setConvergenceHistory()
+solver.solve()
+
+sigma_h, u_h = solution.split()
+sigma_h.rename('Velocity', 'label')
+u_h.rename('Pressure', 'label')
+
+# Plotting velocity field exact solution
+fig, axes = plt.subplots()
+collection = quiver(sigma_e, axes=axes, cmap='coolwarm')
+fig.colorbar(collection)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Exact solution for velocity")
+plt.savefig("exact_velocity.png")
+# plt.show()
+
+# Plotting pressure field exact solution
+fig, axes = plt.subplots()
+collection = tripcolor(exact_solution, axes=axes, cmap='coolwarm')
+fig.colorbar(collection)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Exact solution for pressure")
+plt.savefig("exact_pressure.png")
+# plt.show()
+
+# Plotting velocity field numerical solution
+fig, axes = plt.subplots()
+collection = quiver(sigma_h, axes=axes, cmap='coolwarm')
+fig.colorbar(collection)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("solution_velocity.png")
+# plt.show()
+
+# Plotting pressure field numerical solution
+fig, axes = plt.subplots()
+collection = tripcolor(u_h, axes=axes, cmap='coolwarm')
+fig.colorbar(collection)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("solution_pressure.png")
+# plt.show()
